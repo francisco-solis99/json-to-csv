bb7752f807b2d35347e51c94f8237056230970b2
francisco-solis99 <franciscosoliscss@gmail.com>
2022-07-07 20:11:20 -0500

    [Add] the styles for the header

--- src/main.js
+++ src/main.js
@@ -3,3 +3,11 @@ import {routes}  from './router/routes.js';
 
 const myRouter = new Router(routes);
 console.log(myRouter);
+
+
+// toggle the menu
+const menuBtn = document.querySelector('.menu__button');
+const menu = document.querySelector('.menu__list');
+menuBtn.addEventListener('click', () => menu.classList.toggle('active'));
+
+
