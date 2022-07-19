8b190a13aff7f46687ddfd656e3e3b605feb14cf
francisco-solis99 <franciscosoliscss@gmail.com>
2022-07-18 22:26:27 -0500

    Updates

---
+++ home.098dbee9.js
@@ -0,0 +1,60 @@
+var a=`
+<section class="hero__wrapper" data-aos="fade-up">
+  <div class="hero__cta">
+    <h1 class="hero__title">Translate as a thunder \u26A1</h1>
+    <p class="hero__copy">
+      Make and download your translates from Json to Csv as fast as a lightning-striking to earth.
+    </p>
+    <a href="/generate" class="cta">Get Started</a>
+  </div>
+  <div class="hero__image">
+    <figure class="hero__img-wrapper">
+      <img class="hero__img" src="../../assets/images/json.svg" alt="JSON file">
+    </figure>
+    <img src="../../assets/icons/arrowVec.svg" alt="" class="hero__arrow">
+    <figure class="hero__img-wrapper">
+      <img class="hero__img" src="../../assets/images/csv.svg" alt="">
+    </figure>
+  </div>
+</section>
+
+<section class="features__wrapper" data-aos="fade-up">
+  <div class="features__info">
+    <h2 class="features__principal-title">
+      Start <strong class="title__linear">Easily</strong>
+    </h2>
+    <p class="features__copy">
+      You don't need to read or make a lot of steps to convert your files,  just choose one way to do it, and shazam you get it.
+    </p>
+  </div>
+  <div class="features__list">
+    <article class="features__item" data-aos="zoom-in-up">
+      <figure class="feature__image-wrapper">
+        <img class="feature__image" src="../../assets/images/keyboard.svg" alt="keyboard feature 1">
+      </figure>
+      <h3 class="feature__title">By Typing</h3>
+      <p class="feature__description">
+        You can generate CSV files by typing a JSON file and then you can copy or download it
+      </p>
+    </article>
+    <article class="features__item" data-aos="zoom-in-up">
+      <figure class="feature__image-wrapper">
+        <img class="feature__image" src="../../assets/images/drop.svg" alt="Drop feature 2">
+      </figure>
+      <h3 class="feature__title">Drag and Drop</h3>
+      <p class="feature__description">
+        If you already have a JSON in your files, you can drag and drop your JSON file and download your CSV file.
+      </p>
+    </article>
+  </div>
+</section>
+
+<section class="final-cta__wrapper" data-aos="fade-up">
+  <h2 class="final-cta" data-aos="fade-down">Well, it's time to convert</h2>
+  <a href="/generate" class="cta" data-aos="fade-up">Get Started</a>
+</section>
+
+<footer class="footer__wrapper" data-aos="fade-down" data-aos-duration="1000">
+  <p class="footer__text">Made with \u{1F9E1} by Francisco Solis</p>
+</footer>
+`;var t=()=>{const e=document.createElement("div");return e.innerHTML=a,e};export{t as default};
