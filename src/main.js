6c7294ca467dfd9f1f85bc601759529ea183d46e
francisco-solis99 <franciscosoliscss@gmail.com>
2022-08-11 00:53:59 -0500

    [Fix] the menu toggle and remove teh animation in the menu items

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import Router from "./router/router.js";
 import {routes}  from './router/routes.js';
 
 
-// AOS librarie
+// AOS library
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 AOS.init({
@@ -39,21 +39,26 @@ const menuBtn = document.querySelector('.menu__button');
  * @type {HTMLElement}
  */
 const menu = document.querySelector('.menu__list');
-
+const linksElements = document.querySelectorAll('.menu__item a');
 /**
 * Event reporting the click into the menu button
 *
 * @event MenuBtn-click
 *
 */
-menuBtn.addEventListener('click',
+menuBtn.addEventListener('click', toggleMenu);
+linksElements.forEach(link => link.addEventListener('click', toggleMenu));
+
 /**
 * Toggle the class active in the Menu element
 *
 * @method
 * @fires MenuBtn-click
 */
-() => menu.classList.toggle('active'));
+function toggleMenu() {
+  menu.classList.toggle('active');
+  menuBtn.querySelector('.menu__img').classList.toggle('light');
+}
 
 // when hash changes, load the correct view (with hash because github pages doesn't support dynamic routes) this is better in the branch main
 window.addEventListener('hashchange', () => myRouter.init());
